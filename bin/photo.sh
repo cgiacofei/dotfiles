03eab124e335a753b8cc90bef27fff85faef5135
chris <c.giacofei@gmail.com>
2017-08-24 07:30:41 -0400

    Folders by archive date instead of taken date.

--- bin/photo.sh
+++ bin/photo.sh
@@ -6,6 +6,9 @@ CARRIE_INPUT=$HOME/files/photos/carrie-camera
 
 OUTPUT_DIR=$HOME/files/photos/combined
 
-$EXE -r -d $OUTPUT_DIR/%Y/%m/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CHRIS_INPUT
-$EXE -r -d $OUTPUT_DIR/%Y/%m/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CARRIE_INPUT
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+
+$EXE -r -d $OUTPUT_DIR/$YEAR/$MONTH/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CHRIS_INPUT
+$EXE -r -d $OUTPUT_DIR/$YEAR/$MONTH/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CARRIE_INPUT
 
